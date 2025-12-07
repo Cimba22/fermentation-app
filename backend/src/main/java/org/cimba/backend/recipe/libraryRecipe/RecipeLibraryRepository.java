a73225a55404c6d87d1184d79333343ef253dea1
Oleksandra Mala <oleksandra.s.mala@gmail.com>
2025-12-07 16:34:07 +0100

    Query for search method

--- backend/src/main/java/org/cimba/backend/recipe/libraryRecipe/RecipeLibraryRepository.java
+++ backend/src/main/java/org/cimba/backend/recipe/libraryRecipe/RecipeLibraryRepository.java
@@ -1,10 +1,18 @@
 package org.cimba.backend.recipe.libraryRecipe;
 
-import org.cimba.backend.recipe.recipeUser.RecipeUser;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface RecipeLibraryRepository extends JpaRepository<RecipeLibrary, Long> {
-    Page<RecipeLibrary> search(String lowerCase, Pageable pageable);
+    @Query("""
+        SELECT r FROM RecipeLibrary r
+        WHERE LOWER(r.name) LIKE %:text%
+           OR LOWER(r.description) LIKE %:text%
+    """)
+    Page<RecipeLibrary> search(@Param("text") String text, Pageable pageable);
 }
+
